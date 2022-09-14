9b5364d73b046cc1a1860ab489f55d747d0a777c
caozhi <929877449@qq.com>
2022-09-14 20:35:11 +0800

    difference

--- lodash/FirstNameCao-lodash.js
+++ lodash/FirstNameCao-lodash.js
@@ -1,7 +1,7 @@
 var fristnamecao = {
   //创建一个元素数组，这些元素按大小的长度分成数组。
   //如果数组不能被均匀分割，那么最后的块将是剩余的元素。
-  chunk: (Array, Size) => {
+  chunk: function chunk(Array, Size) {
     if (Size >= Array.length || Size == 0) {
       //判断长度
       return Array;
@@ -25,7 +25,7 @@ var fristnamecao = {
     return NewArray;
   },
 
-  compact: (Array) => {
+  compact:function compact (Array) {
     //创建一个删除了所有假值的数组。值 false、 null、0、“”、 unDefinition 和 NaN 都是假值。
     let NewArray = [];
     for (let i = 0; i < Array.length; ++i) {
@@ -42,6 +42,15 @@ var fristnamecao = {
     return NewArray;
   },
 
-  
+  difference: function difference(Array1, Array2) {
+    let NewArray = [];
 
+    for (let num of Array1) {
+      if (!Array2.includes(num)) {
+        NewArray.push(num);
+      }
+    }
+
+    return NewArray;
+  },
 };
